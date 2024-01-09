ab329cfcc7e3b9023f37f9ecfeb814b971c895fa
efrem <efrem430@gmail.com>
2024-01-09 14:01:32 -0600

    menuCards

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    //tabs
 
 const tabs = document.querySelectorAll('.tabheader__item'),
       tabsContent = document.querySelectorAll('.tabcontent'),
@@ -40,6 +41,87 @@ const tabs = document.querySelectorAll('.tabheader__item'),
         }
 
     });
+
+    //Menu Cards
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes){
+            // this.menuItem = document.querySelector('.menu__item');
+            this.src = src; 
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.classes = classes;
+            this.parent = document.querySelector(parentSelector);
+            this.trasfer = 27;
+            this.changetoUSD();
+
+        }
+        changetoUSD(){
+            this.price = this.price * this.trasfer;
+        }
+        
+        render(){
+            const element = document.createElement('div');
+            
+            element.innerHTML = `
+            <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+            `;
+            this.parent.append(element);
+        }
+        
+        gettitle(){
+            return this.menuCard.querySelector('.menu__item-subtitle').innerText;
+        }
+        settitle(newtitle){
+            this.menuCard.querySelector('.menu__item-subtitle').innerText = newtitle;
+        }
+    }
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container'
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        14,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        21,
+        ".menu .container"
+    ).render();
+    
+
+
+
+
+
+
+
 
 
 const deadline = '2024-01-11';
